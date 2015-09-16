d4a77cc4816b27e71eb755fc6da5e77c2302a150
uade <uade@503PC17.labs>
2015-09-16 19:59:46 -0300

    commit de prueba

---
+++ TP/src/Cliente.java
@@ -0,0 +1,15 @@
+
+public class Cliente {
+	
+	private String domicilio;
+	private String dni;
+	private String mail;
+	private String telefono;
+	
+	public Cliente(String dni, String domicilio, String mail) {
+		this.dni = dni;
+		this.domicilio = domicilio;
+		this.mail = mail;
+	}
+
+}
